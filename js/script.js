249f8f9556795ac94c648841ff3c517000abb753
Evans Roy <evansroysir@gmail.com>
2023-08-12 23:12:51 +0300

    Initial Commit  with Web site structure

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active');
+  loginForm.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+  navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+  navbar.classList.remove('active');
+  loginForm.classList.remove('active');
+}
+
+var swiper = new Swiper(".review-slider",{
+  spaceBetween: 20,
+  centeredSlides: true,
+  grabCursor: true,
+  autoplay:{
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// Get the current year
+var currentYear = new Date().getFullYear();
+  
+// Update the placeholder element with the current year and copyright symbol
+document.getElementById("currentYear").textContent = "\u00A9 " + currentYear + " Lwandeti Blessed Academy";
+
+var swiper = new Swiper('.slider-container', {
+  autoplay: {
+    delay: 5000, // Change slide every 5 seconds
+  },
+  loop: true, // Enable continuous loop
+});
